38e7f3afdbaab36c74316da519b14a2e51df5a57
Aditya Tomar <aditya.tom09@gmail.com>
2026-04-11 22:32:46 +1000

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+// In-memory room storage
+const roomState = {};
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
+
+    if (!roomState[roomId]) {
+      roomState[roomId] = {
+        code: "// Start coding here",
+        language: "javascript"
+      };
+    }
+
+    socket.emit("load_state", roomState[roomId]);
+  });
+
+  socket.on("code_change", ({ roomId, code }) => {
+    if (!roomState[roomId]) return;
+
+    roomState[roomId].code = code;
+    socket.to(roomId).emit("receive_code", code);
+  });
+
+  socket.on("language_change", ({ roomId, language }) => {
+    if (!roomState[roomId]) return;
+
+    roomState[roomId].language = language;
+    socket.to(roomId).emit("receive_language", language);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Collaborative Code Editor backend is running.");
+});
+
+server.listen(5000, () => {
+  console.log("Server listening on http://localhost:5000");
+});
